c2c893d7d789630939d096ac14a85aa4bcb764a5
dfsogijuds89gfusf <120130990+dfsogijuds89gfusf@users.noreply.github.com>
2023-02-19 07:45:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 import { handleEvent } from 'flareact'
 import { processCronTrigger } from './src/functions/cronTrigger'
 
-/**
- * The DEBUG flag will do two things that help during development:
- * 1. we will skip caching on the edge, which makes it easier to
- *    debug.
- * 2. we will return an error message on exception in your Response rather
- *    than the default 404.html page.
- */
+
 const DEBUG = false
 
 addEventListener('fetch', (event) => {
