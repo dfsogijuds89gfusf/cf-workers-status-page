c546c7e852a60765dd1daec3250bd9fba0f48325
dfsogijuds89gfusf <120130990+dfsogijuds89gfusf@users.noreply.github.com>
2023-02-19 06:27:20 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -90,13 +90,13 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
         })}
         <div className="flex flex-row justify-between mt-4 text-sm">
           <div>
-            Powered by{' '}
+            {' '}
             <a href="https://workers.cloudflare.com/" target="_blank">
-              Cloudflare Workers{' '}
+              {' '}
             </a>
-            &{' '}
+            {' '}
             <a href="https://flareact.com/" target="_blank">
-              Flareact{' '}
+              {' '}
             </a>
           </div>
           <div>
@@ -104,7 +104,7 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
               href="https://github.com/eidam/cf-workers-status-page"
               target="_blank"
             >
-              Get Your Status Page
+              
             </a>
           </div>
         </div>
