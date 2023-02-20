e12f220ac40de2174919708ed3aba243df617af5
dfsogijuds89gfusf <120130990+dfsogijuds89gfusf@users.noreply.github.com>
2023-02-19 23:04:30 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,30 @@
-import { Store } from 'laco';
-import { useStore } from 'laco-react';
-import Head from 'flareact/head';
+import { Store } from 'laco'
+import { useStore } from 'laco-react'
+import Head from 'flareact/head'
 
-import { getKVMonitors, useKeyPress } from '../src/functions/helpers';
-import config from '../config.yaml';
-import MonitorCard from '../src/components/monitorCard';
-import MonitorFilter from '../src/components/monitorFilter';
-import MonitorStatusHeader from '../src/components/monitorStatusHeader';
-import ThemeSwitcher from '../src/components/themeSwitcher';
-import '/public/tailwind.css';
+import { getKVMonitors, useKeyPress } from '../src/functions/helpers'
+import config from '../config.yaml'
+import MonitorCard from '../src/components/monitorCard'
+import MonitorFilter from '../src/components/monitorFilter'
+import MonitorStatusHeader from '../src/components/monitorStatusHeader'
+import ThemeSwitcher from '../src/components/themeSwitcher'
 
 const MonitorStore = new Store({
   monitors: config.monitors,
   visible: config.monitors,
   activeFilter: false,
-});
+})
 
 const filterByTerm = (term) =>
   MonitorStore.set((state) => ({
     visible: state.monitors.filter((monitor) =>
-      monitor.name.toLowerCase().includes(term)
+      monitor.name.toLowerCase().includes(term),
     ),
-  }));
+  }))
 
 export async function getEdgeProps() {
   // get KV data
-  const kvMonitors = await getKVMonitors();
+  const kvMonitors = await getKVMonitors()
 
   return {
     props: {
@@ -33,27 +32,50 @@ export async function getEdgeProps() {
       kvMonitors: kvMonitors ? kvMonitors.monitors : {},
       kvMonitorsLastUpdate: kvMonitors ? kvMonitors.lastUpdate : {},
     },
+    // Revalidate these props once every x seconds
     revalidate: 5,
-  };
+  }
 }
 
-export default function Index({
-  config,
-  kvMonitors,
-  kvMonitorsLastUpdate,
-}) {
-  const state = useStore(MonitorStore);
-  const slash = useKeyPress('/');
+export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
+  const state = useStore(MonitorStore)
+  const slash = useKeyPress('/')
 
   return (
-    <div>
+    <div className="min-h-screen">
+      <Head>
+        <title>{config.settings.title}</title>
+        <link rel="stylesheet" href="./style.css" />
+        <script>
+          {`
+          function setTheme(theme) {
+            document.documentElement.classList.remove("dark", "light")
+            document.documentElement.classList.add(theme)
+            localStorage.theme = theme
+          }
+          (() => {
+            const query = window.matchMedia("(prefers-color-scheme: dark)")
+            query.addListener(() => {
+              setTheme(query.matches ? "dark" : "light")
+            })
+            if (["dark", "light"].includes(localStorage.theme)) {
+              setTheme(localStorage.theme)
+            } else {
+              setTheme(query.matches ? "dark" : "light")
+            }
+          })()
+          `}
+        </script>
+      </Head>
       <div className="container mx-auto px-4">
         <div className="flex flex-row justify-between items-center p-4">
           <div className="flex flex-row items-center">
             <img className="h-8 w-auto" src={config.settings.logo} />
-            <h1 className="ml-4 text-3xl">
-              {config.settings.title}
-            </h1>
+            <h1 className="ml-4 text-3xl">{config.settings.title}</h1>
+          </div>
+          <div className="flex flex-row items-center">
+            {typeof window !== 'undefined' && <ThemeSwitcher />}
+            <MonitorFilter active={slash} callback={filterByTerm} />
           </div>
         </div>
         <MonitorStatusHeader kvMonitorsLastUpdate={kvMonitorsLastUpdate} />
@@ -64,26 +86,29 @@ export default function Index({
               monitor={monitor}
               data={kvMonitors[monitor.id]}
             />
-          );
+          )
         })}
         <div className="flex flex-row justify-between mt-4 text-sm">
           <div>
             {' '}
-            <a href="https://ronkkeli.com" target="_blank">
-              {''}
+            <a href="https://ronkkeli.com/" target="_blank">
+              {' '}
             </a>
             {' '}
-            <a href="https://ronkkeli.com" target="_blank">
-              {''}
+            <a href="https://ronkkeli.com/" target="_blank">
+              {' '}
             </a>
           </div>
           <div>
-            <a href="https://ronkkeli.com" target="_blank">
-              {''}
+            <a
+              href="https://ronkkeli.com/"
+              target="_blank"
+            >
+              
             </a>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
